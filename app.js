b61139cef84c0b4a2bb7bc8c60e3d6f39a1bf088
Konstantin Nivnikov <nivnikoff@gmail.com>
2022-11-15 18:51:01 +0300

    pw13 routes and controllers for users added but do not run correctly

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use('/users', require('./routes/users'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
---
+++ controllers/users.js
@@ -0,0 +1,26 @@
+const User = require('../models/user');
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Что-то пошло не так в getUsers' }));
+};
+
+const getUser = (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => res.send({ data: user }))
+    .catch(() => res.status(500).send({ message: 'Что-то пошло не так в getUser' }));
+};
+
+const createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then((users) => res.send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Что-то пошло не так в createUser' }));
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  createUser,
+};
