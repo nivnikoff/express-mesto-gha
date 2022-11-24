0906c73dc36e9ffa6b56f92eb4909fe87064b88a
Konstantin Nivnikov <nivnikoff@gmail.com>
2022-11-25 00:12:43 +0300

    PW14 post-review fixes

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = new mongoose.Schema(
   {
@@ -11,6 +12,10 @@ const cardSchema = new mongoose.Schema(
     link: {
       type: String,
       required: true,
+      validate: {
+        validator: (link) => validator.isUrl(link),
+        message: 'Неверный формат ссылки карточки',
+      },
     },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
--- models/user.js
+++ models/user.js
@@ -18,6 +18,10 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+      validate: {
+        validator: (avatar) => validator.isUrl(avatar),
+        message: 'Неверный формат ссылки аватара',
+      },
     },
     email: {
       type: String,
