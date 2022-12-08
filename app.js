b40ebf367785b58a027e90c114fc84342c343f62
Konstantin Nivnikov <nivnikoff@gmail.com>
2022-12-08 23:45:55 +0300

    PW15 auth middle routes fix

--- app.js
+++ app.js
@@ -75,9 +75,8 @@ app.post('/signup', celebrate({
   }),
 }), createUser);
 
-app.use(auth);
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+app.use('/users', auth, require('./routes/users'));
+app.use('/cards', auth, require('./routes/cards'));
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница по указанному маршруту не найдена'));
