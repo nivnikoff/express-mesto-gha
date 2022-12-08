f4c45070e36df1c02533226041d7d9aff490afb2
Konstantin Nivnikov <nivnikoff@gmail.com>
2022-12-08 11:21:14 +0300

    PW15 commit before adding back to VM

--- models/card.js
+++ models/card.js
@@ -22,13 +22,11 @@ const cardSchema = new mongoose.Schema(
       ref: 'user',
       required: true,
     },
-    likes: [
-      {
-        type: [mongoose.Schema.Types.ObjectId],
-        ref: 'user',
-        default: [],
-      },
-    ],
+    likes: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      default: [],
+    }],
     createdAt: {
       type: Date,
       default: Date.now,
